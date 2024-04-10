68af23a5b1d779e0be3ad91ffffec480f110f042
devnull03 <56480041+devnull03@users.noreply.github.com>
2024-04-10 12:48:59 -0700

    deploy

---
+++ _app/immutable/nodes/2.dZUp7JJk.js
@@ -0,0 +1,31 @@
+import{s as D,a as S,n as j,r as z,b as A}from"../chunks/scheduler.NR01Uj1I.js";import{S as V,i as H,e as f,s as I,c as p,f as k,d as h,j as T,g as q,a as b,m as E,b as x,k as _,n as C}from"../chunks/index.3EpwNQU8.js";function L(l){let t,n,e,c,i,u=`<div id="title" class="h-screen flex justify-center items-center -translate-y-[50vh]"><h1 class="text-6xl text-[#ffffff] font-bold flex h-full w-full items-center justify-center text-center bg-[#5353537f]">Timeline of Discriminatory Laws: A South Asian History</h1></div> <div id="about" class="section svelte-1nbu9gr" style="background-image: url(./text-bg.jpg);"><h2 class="svelte-1nbu9gr">About</h2> <p class="svelte-1nbu9gr">Canada, known for its multicultural society and commitment to human
+        rights, has a history marked by discriminatory laws, policies, and
+        practices targeted at various communities. This platform aims to shed
+        light on the development of racist laws and practices against South
+        Asians in Canada. Exploring the historical records, we seek to
+        illuminate the adversities, obstacles, and victories of the South Asian
+        community in dealing with systemic prejudice.</p></div> <div id="section1" class="section svelte-1nbu9gr" style="background-image: url(./text-bg.jpg);"><h2 class="svelte-1nbu9gr">Immigration Act 1906 and continuous journey Act 1908</h2> <p class="svelte-1nbu9gr">The Immigration Act of 1906 marked a significant shift towards stricter
+        immigration policies. It broadened the list of forbidden immigrant
+        categories, established a formalized deportation procedure, and granted
+        the government expanded authority to exercise discretionary judgments
+        regarding admission.<br/><br/>
+        An amendment to the Immigration Act was made in 1908 to incorporate the &quot;continuous
+        journey regulation&quot; of the Canadian government, which forbade the entry into
+        Canada of any immigrant who had not traveled continuously from the place
+        of their birth or citizenship. As most voyages from the region involved stops,
+        this posed a considerable obstacle to immigrants from South Asia.</p></div> <div id="section2" class="section svelte-1nbu9gr" style="background-image: url(./text-bg.jpg);"><h2 class="svelte-1nbu9gr">Dominion Elections Act</h2> <p class="svelte-1nbu9gr">In 1920, the Dominion Elections Act resulted in the removal of federal
+        voting privileges for those who had been denied the right to vote at the
+        provincial level due to their ethnicity, particularly impacting people
+        of Chinese, Japanese, and South Asian descent in British Columbia.
+        Moreover, individuals of Chinese origin in Saskatchewan were also denied
+        their right to vote.</p></div> <div id="section3" class="section svelte-1nbu9gr" style="background-image: url(./text-bg.jpg);"><h2 class="svelte-1nbu9gr">The Komagata Maru Incident</h2> <p class="svelte-1nbu9gr">This incident unfolded during the spring and summer of 1914, just before
+        the onset of the First World War. It turned out to be a harrowing and
+        sorrowful ordeal for the passengers involved. Initially, there was an
+        unsuccessful attempt at confrontation with officials, police, and the
+        military at the Port of Vancouver. Subsequently, as the passengers
+        returned to India, they faced a tragic and fatal clash with police and
+        troops near Kolkata. The Komagata Maru tale illustrates the Canadian
+        government&#39;s purposeful exclusionary policy toward ethnic groups it
+        deemed unfit to enter. These defenses of Canada&#39;s continued status as a
+        &quot;White Man&#39;s Country&quot; were couched in racist and ethnocentric notions of
+        &quot;progress,&quot; &quot;civilization,&quot; and &quot;suitability.&quot;</p></div>`,d,s,v='<h2 class="text-4xl font-semibold border-b-2 pb-6 w-[80%] text-center">A Timeline of Discriminatory laws</h2> <div id="timeline" class="flex items-center justify-center h-screen w-full"><iframe src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=132DfhxF5iJTrjHiVMfGkVyMojIg2gbqaViMSO1Uu_kY&amp;font=Default&amp;lang=en&amp;initial_zoom=2&amp;height=650" width="85%" height="650" allowfullscreen="" frameborder="0" title="Timeline"></iframe></div>',m,y;return S(l[5]),{c(){t=f("main"),n=f("div"),e=f("div"),c=I(),i=f("div"),i.innerHTML=u,d=I(),s=f("div"),s.innerHTML=v,this.h()},l(a){t=p(a,"MAIN",{class:!0});var o=k(t);n=p(o,"DIV",{class:!0});var w=k(n);e=p(w,"DIV",{class:!0,style:!0}),k(e).forEach(h),w.forEach(h),c=T(o),i=p(o,"DIV",{class:!0,"data-svelte-h":!0}),q(i)!=="svelte-go0nb5"&&(i.innerHTML=u),o.forEach(h),d=T(a),s=p(a,"DIV",{class:!0,"data-svelte-h":!0}),q(s)!=="svelte-16zoaus"&&(s.innerHTML=v),this.h()},h(){b(e,"class","background-container bg-cover w-screen h-screen bg-center transition-all duration-500 ease-in-out svelte-1nbu9gr"),E(e,"background-image","url(./img4.jpg)"),b(n,"class","sticky top-0 left-0 overflow-clip -z-50"),b(i,"class","z-50 flex flex-col justify-start -translate-y-[50vh]"),b(t,"class","relative"),b(s,"class","flex flex-col items-center justify-center py-20")},m(a,o){x(a,t,o),_(t,n),_(n,e),l[6](e),_(t,c),_(t,i),l[7](i),x(a,d,o),x(a,s,o),m||(y=[C(window,"scroll",l[4]),C(window,"resize",l[5])],m=!0)},p:j,i:j,o:j,d(a){a&&(h(t),h(d),h(s)),l[6](null),l[7](null),m=!1,z(y)}}}function P(l,t,n){let e,c,i,u;const d={title:"./img4.jpg",about:"./img4.jpg",section1:"./img1.jpg",section2:"./img2.jpg",section3:"./img3.jpg"};let s="title";const v=r=>{let g=m();Object.keys(d).includes(g)&&g!==s&&(y(d[g]),s=g)},m=()=>document.elementsFromPoint(u/2,i/2).find(M=>M.id)?.id,y=r=>{e.style.getPropertyValue("background-image")!==`url(${r})`&&e?.style.setProperty("background-image",`url(${r})`)};function a(){n(2,i=window.innerHeight),n(3,u=window.innerWidth)}function o(r){A[r?"unshift":"push"](()=>{e=r,n(0,e)})}function w(r){A[r?"unshift":"push"](()=>{c=r,n(1,c)})}return[e,c,i,u,v,a,o,w]}class K extends V{constructor(t){super(),H(this,t,P,L,D,{})}}export{K as component};
